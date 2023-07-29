5725b09448aca4e8128222cb82ab6a3323d60d69
Mohd Sameer <mesh789736@gmail.com>
2023-07-29 15:39:54 -0600

    Updates

---
+++ precache-manifest.3dcdf6b2acd66932b423740048a8de7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d4dd37cc61e4d4538d1",
+    "url": "/newsapp/static/css/main.b846a606.chunk.css"
+  },
+  {
+    "revision": "6d4dd37cc61e4d4538d1",
+    "url": "/newsapp/static/js/main.6d4dd37c.chunk.js"
+  },
+  {
+    "revision": "28be38ec31965621a8d7",
+    "url": "/newsapp/static/js/1.28be38ec.chunk.js"
+  },
+  {
+    "revision": "a68a04643953e8e2c8a9",
+    "url": "/newsapp/static/js/runtime~main.a68a0464.js"
+  },
+  {
+    "revision": "b429e02612b7a4ea70ee1d9ca76b5c96",
+    "url": "/newsapp/static/media/spinner.b429e026.gif"
+  },
+  {
+    "revision": "01fc4a433f00fc1193faf8db922a9c71",
+    "url": "/newsapp/index.html"
+  }
+];
